022c29aaaac8feffccbfa957cab687e482beb0bf
toainq <nqtoai2181@gmail.com>
2024-12-11 10:55:32 +0700

    fix deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,7 +61,7 @@ export default async function RootLayout({
           showAtBottom={false}
         />
         <ScrollToTop />
-        <Navbar categories={categories} />
+        <Navbar categories={categories || []} />
         {children}
         <ClientOnly>
           <SpeedDialComponent />

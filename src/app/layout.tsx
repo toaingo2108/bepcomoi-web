b9382384cf529f57f1099d0b3e3552cc1ef8b90b
toainq <nqtoai2181@gmail.com>
2024-12-13 17:26:18 +0700

    update seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,8 +36,11 @@ export const metadata: Metadata = {
   ],
   icons: [
     {
-      url: "/logo.png",
-      sizes: "32x32",
+      url: "/favicon-16x16.png",
+      type: "image/png",
+    },
+    {
+      url: "/favicon-32x32.png",
       type: "image/png",
     },
   ],
--- src/app/san-pham/[slug]/layout.tsx
+++ src/app/san-pham/[slug]/layout.tsx
@@ -1,5 +1,36 @@
+import { getDetailProduct } from "@/lib/api";
+import { Product } from "@/types/product";
+import { Metadata, ResolvingMetadata } from "next";
 import Image from "next/image";
 import React from "react";
+
+export async function generateMetadata(
+  { params }: { params: { slug: string } },
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const slug = params.slug;
+
+  // fetch data
+  const product = await getDetailProduct(slug);
+
+  if (!product) {
+    return {
+      title: "Not Found",
+    };
+  }
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: product.name,
+    description: product.name + " - " + "Bếp Có Mồi",
+    openGraph: {
+      images: [process.env.NEXT_PUBLIC_IMAGE_BASE_URL + product?.images?.[0], ...previousImages],
+    },
+  };
+}
 
 interface ProductDetailLayoutProps {
   children: React.ReactNode;
@@ -9,22 +40,10 @@ const ProductDetailLayout = ({ children }: ProductDetailLayoutProps) => {
     <>
       {children}
       <div className="aspect-[1908/600] relative mt-10">
-        <Image
-          src="/home1.jpg"
-          alt=""
-          fill
-          className="object-contain"
-          priority
-        />
+        <Image src="/home1.jpg" alt="" fill className="object-contain" priority />
       </div>
       <div className="aspect-[1908/420] relative">
-        <Image
-          src="/sliderdesktop1.jpg"
-          alt=""
-          fill
-          className="object-contain"
-          priority
-        />
+        <Image src="/sliderdesktop1.jpg" alt="" fill className="object-contain" priority />
       </div>
     </>
   );
